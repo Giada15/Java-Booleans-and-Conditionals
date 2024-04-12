709ad365286127b7f8e150cba43f53168b3a13d6
Giada15 <tascheriogiada@gmail.com>
2024-04-12 09:53:43 -0400

    Created basic bank app to practice control flow structures with if-else statements

---
+++ Bank.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\n****ROYAL BANK OF JAVA****");
+        System.out.println("Are you here to get a mortgage? (yes or no)");
+        String decision = scan.nextLine();
+        if(decision.equals("yes")){
+            System.out.println("\nGreat! In one line" +
+                   "\nHow much money do you have in your savings?" +
+                   "\nAnd, how much do you owe in credit card debt?");
+        double savings = scan.nextDouble();
+        double cardDebt = scan.nextDouble();
+
+        System.out.println("How many years have you worked for");
+        double years = scan.nextDouble();
+
+        System.out.println("What is your name?");
+        scan. nextLine();
+        String name = scan.nextLine();
+
+        if(savings>=10000 && cardDebt<5000 && years>2){
+            System.out.println("Congratulations "+name+", you have been approved!");
+        }else{
+            System.out.println("Sorry, you are not eligible for a mortgage.");
+        }
+
+        }else {
+            System.out.println("OK. Have a nice day!");
+        }
+    }
+    
+}
